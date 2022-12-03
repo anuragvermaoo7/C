10a7f4073fa7f64044bb5f442d844ec913c0204f
Anurag Verma <118360572+anuragvermaoo7@users.noreply.github.com>
2022-12-03 10:48:25 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	long int x,y,sum;
+	
+	printf ("Enter first Number");
+	scanf ("%d",&x);
+	
+	printf ("Enter second Number");
+	scanf ("%d",&y);
+	
+	sum=x+y;
+	
+	printf("the sum is %d",sum);
+	getch();
+}
